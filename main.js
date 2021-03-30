245e3902afc240835115fa9eb9d0b8cff699df87
oliver tuuling <oliver.tuuling@tptlive.ee>
2021-03-30 14:54:10 +0300

    bad blocks are being created

--- main.js
+++ main.js
@@ -1,103 +1,174 @@
 let deg2rad = (deg) => {
-    return deg * (Math.PI / 180);
-  };
-  
-  let rad2deg = (rad) => {
-    return rad * (180 / Math.PI);
-  };
-  
-  window.addEventListener("load", () => {
-    let el = document.querySelector("#canvas");
-    el.width = window.innerWidth;
-    el.height = window.innerHeight;
-    let ctx = el.getContext("2d");
-    let orbs = [];
-    for (let i = 0; i < 1; i++) {
-      let x = Math.floor(el.width / 2);
-      let y = Math.floor(el.height / 1.1);
-      let angle = Math.floor(Math.random() * 180);
-      orbs.push({ x: x, y: 600, angle: angle });
-    }
+  return deg * (Math.PI / 180);
+};
 
-    var player = {
-      x:Math.floor(el.width / 2.2),
-      y:Math.floor(el.height / 1.07),
-      speed: 2
-    }
+let rad2deg = (rad) => {
+  return rad * (180 / Math.PI);
+};
 
-    function drawPlayer(x,y) {
-      var x = player.x;
-      var y = player.y;
-      ctx.fillStyle = "#00FF";
+window.addEventListener("load", () => {
+  let el = document.querySelector("#canvas");
+  el.width = window.innerWidth;
+  el.height = window.innerHeight;
+  let ctx = el.getContext("2d");
+  let orbs = [];
+  for (let i = 0; i < 1; i++) {
+    let x = Math.floor(el.width / 2);
+    let y = Math.floor(el.height / 1.1);
+    let angle = Math.floor(Math.random() * -180);
+    orbs.push({ x: x, y: 200, angle: angle });
+  }
+
+  var colors = ["blue", "green", "yellow", "orange", "red"]
+
+  let row1 = [13];
+  for (let i = 0; i < 13; i++) {
+    let x = 130 * i;
+    let y = Math.floor(el.height / 8);
+    row1.push({ x: x + 100, y: y, width: 125, height: 75, color:colors[Math.floor(Math.random() * colors.length)] });
+  }
+
+  function createBlocks() {
+    // for (var i = 0; i < row1.length; i++) {
+    //   var row = row1[i];
+    //   ctx.beginPath();
+    //   ctx.rect(row.x + 50, row.y, row.width, row.height);
+    //   ctx.fillStyle = row.color;
+    //   ctx.fill();
+    //   ctx.stroke();
+    // }
+    row1.forEach((block) => {
       ctx.beginPath();
-      ctx.moveTo(player.x,player.y);
-      ctx.fillRect(x, y, 200, 20);
-      ctx.strokeRect(x, y, 200, 20);
+      ctx.fillRect(block.x, block.y, 125, 75);
+      ctx.fillStyle = colors[Math.floor(Math.random() * colors.length)];
       ctx.fill();
-      if(player.x < 0)
-      {
-        LEFT = false;
-      }
-      
-      if(player.x + 200 > el.width)
-      {
-        RIGHT = false;
-      }
       ctx.closePath();
+    });
+  }
+
+  var player = {
+    x: Math.floor(el.width / 2.2),
+    y: Math.floor(el.height / 1.07),
+    speed: 2
+  }
+
+  function drawPlayer(x, y) {
+    var x = player.x;
+    var y = player.y;
+    ctx.fillStyle = "#0000FF";
+    ctx.beginPath();
+    ctx.moveTo(player.x, player.y);
+    ctx.fillRect(x, y, 200, 20);
+    ctx.strokeRect(x, y, 200, 20);
+    ctx.fill();
+    if (player.x < 0) {
+      LEFT = false;
     }
 
-    var LEFT = false; 
-    var RIGHT = false;
-    
-    function move() {    
-      if(LEFT) { 
-        player.x -= player.speed;
+    if (player.x + 200 > el.width) {
+      RIGHT = false;
+    }
+    ctx.closePath();
+  }
+
+  var LEFT = false;
+  var RIGHT = false;
+
+  function move() {
+    if (LEFT) {
+      player.x -= player.speed;
+    }
+    if (RIGHT) {
+      player.x += player.speed;
+    }
+  }
+
+  document.onkeydown = function (e) {
+    if (e.keyCode == 37) LEFT = true;
+    if (e.keyCode == 39) RIGHT = true;
+  }
+
+  document.onkeyup = function (e) {
+    if (e.keyCode == 37) LEFT = false;
+    if (e.keyCode == 39) RIGHT = false;
+  }
+
+  function loadOrb() {
+    orbs.forEach((orb) => {
+      ctx.beginPath();
+      ctx.arc(orb.x, orb.y, 7, 5, 7 * Math.PI);
+      orb.x += Math.cos(deg2rad(orb.angle)) / 3;
+      orb.y += Math.sin(deg2rad(orb.angle));
+      ctx.fillStyle = "gray";
+      ctx.fill();
+      ctx.closePath();
+
+      let distPlayer = Math.abs(orb.x - player.x - 200 / 2) 
+
+      if (orb.x - 5 < 0 || orb.x + 5 > el.width) orb.angle = 180 - orb.angle;
+      if (orb.y - 5 < 0 || orb.y + 5 > el.height) orb.angle = 360 - orb.angle;
+
+      if (orb.y + 5 > player.y && distPlayer < 100)
+      {
+        orb.angle -= 90;
       }
-      if(RIGHT) {
-        player.x += player.speed;	
-      }   
-    }
-    
-    document.onkeydown = function(e) {
-      if(e.keyCode == 37) LEFT = true;
-      if(e.keyCode == 39) RIGHT = true;
-    }
-    
-    document.onkeyup = function(e) {
-      if(e.keyCode == 37) LEFT = false;
-      if(e.keyCode == 39) RIGHT = false;
-    }
 
-    function loadOrb() {
-      orbs.forEach((orb) => {
-        ctx.beginPath();
-        ctx.arc(orb.x, orb.y, 10, 5, 10 * Math.PI);
-        orb.x += Math.cos(deg2rad(orb.angle))/3;
-        orb.y += Math.sin(deg2rad(orb.angle));
-        ctx.fillStyle = "black";
-        ctx.fill();
-        ctx.closePath();
+      for (var i = 0; i < row1.length; i++) {
+        var row = row1[i];
+        let distBlockBall = Math.abs(orb.x - row.x - 125 / 2)
+        if (orb.y - 80 < row.y && distBlockBall < 62.5) {
+          orb.angle -= 90;
+          ctx.clearRect(row.x, row.y, 125, 75);
+          context.beginPath();
+        }  
+      }
 
-        let dist = Math.abs(orb.x - player.x - 200 / 2) 
+      // row1.forEach((block) => {
+      //   let distBlockBall = Math.abs(orb.x - block.x - 125 / 2)
+      //   if (orb.y - 80 < block.y && distBlockBall < 62.5) {
+      //     orb.angle += 90;
+      //     orb.angle %= 360;
+      //     ctx.clearRect(block.x, block.y, 125, 75);
+      //     context.beginPath();
+      //   }  
+      // })
 
-        if (orb.x - 10 < 0 || orb.y - 10 < 0 || orb.x + 10 > el.width || orb.y + 10 > el.height) {
-          orb.angle += 90;
-          orb.angle %= 360;
-        }
-        if (orb.y + 10 > player.y && dist < 100)
-        {
-          orb.angle += 90;
-          orb.angle %= 360;
-        }
-        else{
-        }
-      });
+      if (orb.y > player.y) {
+        console.log("gg")
+      }
+    });
+  }
+
+  function ballAndBlocksColliding(circle, rect) {
+    var distX = Math.abs(circle.x - rect.x - rect.w / 2);
+    var distY = Math.abs(circle.y - rect.y - rect.h / 2);
+
+    if (distX > (rect.w / 2 + circle.r)) {
+        return false;
+    }
+    if (distY > (rect.h / 2 + circle.r)) {
+        return false;
     }
 
-    setInterval(() => {
-      ctx.clearRect(0, 0, el.width, el.height);
-      loadOrb();
-      drawPlayer();
-      move();
-    }, 2);
-  });
+    if (distX <= (rect.w / 2)) {
+        return true;
+    }
+    if (distY <= (rect.h / 2)) {
+        return true;
+    }
+
+    var dx = distX - rect.w / 2;
+    var dy = distY - rect.h / 2;
+    return (dx * dx + dy * dy <= (circle.r * circle.r));
+}
+
+  setInterval(() => {
+    ctx.clearRect(0, 0, el.width, el.height);
+    loadOrb();
+    drawPlayer();
+    move();
+    createBlocks();
+
+  }, 500);
+
+});
